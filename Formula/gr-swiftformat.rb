db0952777328f7eb228deaf3a637aa8054c50c3f
Yuriy Hanysh <yuriy.hanysh@grammarly.com>
2020-09-22 12:09:01 +0300

    Rename formula

--- Formula/gr-swiftformat.rb
+++ Formula/gr-swiftformat.rb
@@ -1,4 +1,4 @@
-class GRSwiftformat < Formula
+class GrSwiftformat < Formula
   desc "Formatting tool for reformatting Swift code"
   homepage "https://github.com/nicklockwood/SwiftFormat"
   url "https://github.com/nicklockwood/SwiftFormat/archive/0.46.3.tar.gz"
